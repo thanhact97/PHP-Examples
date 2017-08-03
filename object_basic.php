550916f8c49a38d3253bc51a404e7638de5710e2
thanhcr <thanhact97@gmail.com>
2017-08-03 10:46:07 +0700

    Day file len da nay

--- object_basic.php
+++ object_basic.php
@@ -1,14 +1,13 @@
 <?php
-//Da php la chi co php,ko chua code html
-            class Person{
-                public $isAlive = true;
-                public $firstname;
-                public $lastname;
-                public $age;
-             
-            }
-                $teacher = new Person();
-                $student = new Person();
-                
-                echo $teacher->isAlive;
-        ?>
+class Person{
+	public $isAlive = true;
+	public $firstname;
+	public $lastname;
+	public $age;
+}
+
+$teacher = new Person();
+$student = new Person();
+
+echo $teacher->isAlive;
+?>
